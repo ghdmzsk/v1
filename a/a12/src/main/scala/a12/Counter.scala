bb4d4e29dbe09b2e30303b04f8767877d2251370
djx314 <djx314@sina.cn>
2022-04-16 22:54:33 +0800

    Godzilla.

--- a/a12/src/main/scala/a12/Counter.scala
+++
@@ -1,9 +0,0 @@
-package a12
-
-trait Number1
-case class Number1S(tail: Number1, head: Number2) extends Number1
-case object Number1T                              extends Number1
-
-trait Number2
-case class Number2S(tail: Number2, head: Number1) extends Number2
-case object Number2T                              extends Number2
