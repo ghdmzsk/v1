29c8e61c45eca1d08f8f92ba25970c68b129f45c
djx314 <djx314@sina.cn>
2022-04-21 01:37:49 +0800

    Pythagoras Godzilla.

--- a/a13/src/main/scala/a13/Counter.scala
+++ a/a13/src/main/scala/a13/Counter.scala
@@ -1,13 +1,31 @@
 package a13
 
-trait Number1
-case class Number1S(num2: () => Number2, num3: () => Number3) extends Number1
-case object Number1T                                          extends Number1
+trait Number1 {
+  def method1(num2: Number2, num3: Number3): Number1
+}
+case class Number1S(tail2: () => Number2, tail3: () => Number3) extends Number1 {
+  def method1(num2: Number2, num3: Number3): Number1 = Number1S(() => tail2().method2(num3), () => tail3().method2(num2))
+}
+case object Number1T extends Number1 {
+  def method1(num2: Number2, num3: Number3): Number1 = Number1S(() => num2, () => num3)
+}
 
-trait Number2
-case class Number2S(num1: Number1, num3: Number3) extends Number2
-case object Number2T                              extends Number2
+trait Number2 {
+  def method2(num3: Number3): Number2
+}
+case class Number2S(tail2: Number2, tail1: Number1) extends Number2 {
+  def method2(num3: Number3): Number2 = Number2S(tail2.method2(num3), tail1.method1(tail2, num3))
+}
+case class Number2T(tail2: () => Number2) extends Number2 {
+  def method2(num3: Number3): Number2 = tail2().method2(num3)
+}
 
-trait Number3
-case class Number3S(num1: Number1, num2: Number2) extends Number3
-case object Number3T                              extends Number3
+trait Number3 {
+  def method2(num2: Number2): Number3
+}
+case class Number3S(tail3: Number3, tail1: Number1) extends Number3 {
+  def method2(num2: Number2): Number3 = Number3S(tail3.method2(num2), tail1.method1(num2, tail3))
+}
+case class Number3T(tail3: () => Number3) extends Number3 {
+  def method2(num2: Number2): Number3 = tail3().method2(num2)
+}
--- a/a13/src/main/scala/a13/Runner.scala
+++ a/a13/src/main/scala/a13/Runner.scala
@@ -2,42 +2,45 @@ package a13
 
 object Runner {
 
-  lazy val numAny: Number1 = Number1S(() => Number2S(Number1S(() => Number2S(numN1, Number3T), () => Number3T), Number3T), () => Number3T)
-  lazy val numN1: Number1  = Number1S(() => Number2S(numN7, Number3S(numN4, Number2T)), () => Number3T)
-  lazy val numN2: Number2  = Number2S(Number1S(() => Number2S(numN5, Number3T), () => Number3T), Number3T)
-  lazy val numN3: Number1  = Number1S(() => Number2S(Number1S(() => Number2T, () => Number3T), Number3T), () => Number3T)
-  lazy val numN4: Number1  = Number1S(() => Number2S(numN7, Number3S(Number1T, numN6)), () => Number3T)
-  lazy val numN5: Number1  = Number1S(() => Number2S(numN7, Number3T), () => Number3T)
-  lazy val numN6: Number2  = Number2S(numN7, Number3T)
+  lazy val numAny: Number1 = Number1S(() => Number2S(numN2, Number1S(() => Number2S(numN2, numN1), () => num3)), () => num3)
+  lazy val num3: Number3   = Number3S(numN8, numN1)
+  lazy val numN1: Number1  = Number1S(() => Number2S(numN2, numN3), () => numN8)
+  lazy val numN2: Number2  = Number2S(numN9, numN3)
+  lazy val numN3: Number1  = Number1S(() => Number2S(numN2, numN4), () => numN8)
+  lazy val numN4: Number1  = Number1S(() => Number2S(numN2, numN5), () => numN8)
+  lazy val numN5: Number1  = Number1S(() => Number2S(numN2, numN5), () => numN8)
+  lazy val numN6: Number2  = Number2S(numN2, numN7)
   lazy val numN7: Number1 =
-    Number1S(() => Number2T, () => Number3S(Number1T, Number2S(Number1S(() => Number2S(Number1T, Number3T), () => Number3T), Number3T)))
+    Number1S(() => Number2T(() => numN2), () => Number3S(Number3S(Number3S(Number3S(numN8, Number1T), Number1T), Number1T), Number1T))
+  lazy val numN8: Number3 = Number3T(() => num3)
+  lazy val numN9: Number2 = Number2T(() => numN2)
 
   case class Count(num1: Int, num2: Int, num3: Int)
 
   def countNumber1(num1: Number1): Count = num1 match {
-    case Number1S(num2, num3) =>
-      val c1 = countNumber2(num2())
-      val c2 = countNumber3(num3())
+    case Number1S(tail2, tail3) =>
+      val c1 = countNumber2(tail2())
+      val c2 = countNumber3(tail3())
       Count(num1 = c1.num1 + c2.num1 + 1, num2 = c1.num2 + c2.num2, num3 = c1.num3 + c2.num3)
     case Number1T =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
   def countNumber2(num2: Number2): Count = num2 match {
-    case Number2S(num1, num3) =>
-      val c1 = countNumber1(num1)
-      val c2 = countNumber3(num3)
+    case Number2S(tail2, tail1) =>
+      val c1 = countNumber2(tail2)
+      val c2 = countNumber1(tail1)
       Count(num1 = c1.num1 + c2.num1, num2 = c1.num2 + c2.num2 + 1, num3 = c1.num3 + c2.num3)
-    case Number2T =>
+    case Number2T(_) =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
   def countNumber3(num3: Number3): Count = num3 match {
-    case Number3S(num1, num2) =>
-      val c1 = countNumber1(num1)
-      val c2 = countNumber2(num2)
+    case Number3S(tail3, tail1) =>
+      val c1 = countNumber3(tail3)
+      val c2 = countNumber1(tail1)
       Count(num1 = c1.num1 + c2.num1, num2 = c1.num2 + c2.num2, num3 = c1.num3 + c2.num3 + 1)
-    case Number3T =>
+    case Number3T(_) =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
