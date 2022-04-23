ade4a2e5e9b30d1704f304e8444fa5fe913e7a21
djx314 <djx314@sina.cn>
2022-04-23 21:06:54 +0800

    Godzilla.

--- a/a13/src/main/scala/a13/Counter.scala
+++ a/a13/src/main/scala/a13/Counter.scala
@@ -1,40 +1,75 @@
 package a13
 
-trait Number1 {
-  def method1(num2: Number2, num3: Number3): Number1
+trait Number1_1 {
+  def method1(num2: Number2_2): Number3_3
 }
-case class Number1S(tail2: Number2, tail3: Number3) extends Number1 {
-  override def method1(num2: Number2, num3: Number3): Number1 = Number1S(num2.method2(tail3), num3.method3(tail2))
+case class Number1_1S(tail2: Number1_2, tail3: Number1_3) extends Number1_1 {
+  override def method1(num2: Number2_2): Number3_3 = Number3_3T // Number3_3S(num2.method2(tail3), num3.method3(tail2))
 }
-case class Number1T(tail1: () => Number1) extends Number1 {
-  override def method1(num2: Number2, num3: Number3): Number1 = tail1().method1(num2, num3)
-}
-case object Number1U extends Number1 {
-  override def method1(num2: Number2, num3: Number3): Number1 = Number1U
+case object Number1_1T extends Number1_1 {
+  override def method1(num2: Number2_2): Number3_3 = Number3_3T
 }
 
-trait Number2 {
-  def method2(num3: Number3): Number2
+trait Number1_2 {
+  def method2(num3: Number2_3): Number3_1
 }
-case class Number2S(tail1: Number1, tail2: Number2) extends Number2 {
-  override def method2(num3: Number3): Number2 = Number2S(tail1.method1(tail2, num3), tail2.method2(num3))
+case class Number1_2S(tail1: Number1_1, tail2: Number1_3) extends Number1_2 {
+  override def method2(num3: Number2_3): Number3_1 = Number3_1T // Number3_1S(tail1.method1(tail2, num3), tail2.method2(num3))
 }
-case class Number2T(tail2: () => Number2) extends Number2 {
-  override def method2(num3: Number3): Number2 = tail2().method2(num3)
-}
-case object Number2U extends Number2 {
-  override def method2(num3: Number3): Number2 = Number2U
+case object Number1_2T extends Number1_2 {
+  override def method2(num3: Number2_3): Number3_1 = Number3_1T
 }
 
-trait Number3 {
-  def method3(num2: Number2): Number3
+trait Number1_3 {
+  def method3(num1: Number2_1): Number3_2
 }
-case class Number3S(tail1: Number1, tail3: Number3) extends Number3 {
-  override def method3(num2: Number2): Number3 = Number3S(tail1.method1(num2, tail3), tail3.method3(num2))
+case class Number1_3S(tail1: Number1_1, tail3: Number1_2) extends Number1_3 {
+  override def method3(num1: Number2_1): Number3_2 = Number3_2T // Number3_2S(tail1.method1(num2, tail3), tail3.method3(num2))
 }
-case class Number3T(tail3: () => Number3) extends Number3 {
-  override def method3(num2: Number2): Number3 = tail3().method3(num2)
+case object Number1_3T extends Number1_3 {
+  override def method3(num1: Number2_1): Number3_2 = Number3_2T
 }
-case object Number3U extends Number3 {
-  override def method3(num2: Number2): Number3 = Number3U
+
+// ====
+trait Number2_1 {
+  def method1(num3: Number1_3): Number3_2
 }
+case class Number2_1S(tail2: Number2_2, tail3: Number2_3) extends Number2_1 {
+  override def method1(num3: Number1_3): Number3_2 = Number3_2T // Number3_2S(num2.method2(tail3), num3.method3(tail2))
+}
+case class Number2_1T(tail1: () => Number2_1) extends Number2_1 {
+  override def method1(num3: Number1_3): Number3_2 = tail1().method1(num3)
+}
+
+trait Number2_2 {
+  def method2(num1: Number1_1): Number3_3
+}
+case class Number2_2S(tail1: Number2_1, tail2: Number2_3) extends Number2_2 {
+  override def method2(num1: Number1_1): Number3_3 = Number3_3T // Number3_3S(tail1.method1(tail2, num3), tail2.method2(num3))
+}
+case class Number2_2T(tail2: () => Number2_2) extends Number2_2 {
+  override def method2(num1: Number1_1): Number3_3 = tail2().method2(num1)
+}
+
+trait Number2_3 {
+  def method3(num2: Number1_2): Number3_1
+}
+case class Number2_3S(tail1: Number2_1, tail3: Number2_2) extends Number2_3 {
+  override def method3(num2: Number1_2): Number3_1 = Number3_1T // Number3_1S(tail1.method1(num2, tail3), tail3.method3(num2))
+}
+case class Number2_3T(tail3: () => Number2_3) extends Number2_3 {
+  override def method3(num2: Number1_2): Number3_1 = tail3().method3(num2)
+}
+
+// ====
+trait Number3_1
+case class Number3_1S(tail2: Number3_2, tail3: Number3_3) extends Number3_1
+case object Number3_1T                                    extends Number3_1
+
+trait Number3_2
+case class Number3_2S(tail1: Number3_1, tail3: Number3_3) extends Number3_2
+case object Number3_2T                                    extends Number3_2
+
+trait Number3_3
+case class Number3_3S(tail1: Number3_1, tail3: Number3_2) extends Number3_3
+case object Number3_3T                                    extends Number3_3
--- a/a13/src/main/scala/a13/Runner.scala
+++ a/a13/src/main/scala/a13/Runner.scala
@@ -6,94 +6,88 @@ object Runner {
 
   def randomGen: Int = math.abs(Random.nextInt() % 10)
 
-  def genNumber1(n: Int, deep: Int, zero1: => Number1, zero2: => Number2, zero3: => Number3): Number1 = {
+  def genNumber1(n: Int, deep: Int, zero1: => Number2_1, zero2: => Number2_2, zero3: => Number2_3): Number2_1 = {
     if (deep > 0)
-      Number1S(genNumber2(n - 1, deep - 1, zero1, zero2, zero3), genNumber3(n - 1, deep - 1, zero1, zero2, zero3))
+      Number2_1S(genNumber2(randomGen, deep - 1, zero1, zero2, zero3), genNumber3(n - 1, deep, zero1, zero2, zero3))
     else
-      Number1T(() => zero1)
+      Number2_1T(() => zero1)
   }
-  def genNumber2(n: Int, deep: Int, zero1: => Number1, zero2: => Number2, zero3: => Number3): Number2 = {
+  def genNumber2(n: Int, deep: Int, zero1: => Number2_1, zero2: => Number2_2, zero3: => Number2_3): Number2_2 = {
     if (n > 0)
-      Number2S(genNumber1(n - 1, deep, zero1, zero2, zero3), genNumber2(n - 1, deep - 1, zero1, zero2, zero3))
+      Number2_2S(genNumber1(randomGen, deep - 1, zero1, zero2, zero3), genNumber3(n - 1, deep, zero1, zero2, zero3))
     else
-      Number2T(() => zero2)
+      Number2_2T(() => zero2)
   }
-  def genNumber3(n: Int, deep: Int, zero1: => Number1, zero2: => Number2, zero3: => Number3): Number3 = {
+  def genNumber3(n: Int, deep: Int, zero1: => Number2_1, zero2: => Number2_2, zero3: => Number2_3): Number2_3 = {
     if (n > 0)
-      Number3S(genNumber1(n - 1, deep, zero1, zero2, zero3), genNumber3(n - 1, deep - 1, zero1, zero2, zero3))
+      Number2_3S(genNumber1(randomGen, deep - 1, zero1, zero2, zero3), genNumber2(n - 1, deep, zero1, zero2, zero3))
     else
-      Number3T(() => zero3)
+      Number2_3T(() => zero3)
   }
 
-  def genNumber1_2(n: Int, deep: Int): Number1 = {
+  def genNumber1_2(n: Int, deep: Int): Number1_1 = {
     if (deep > 0)
-      Number1S(genNumber2_2(randomGen, deep - 1), genNumber3_2(n - 1, deep - 1))
+      Number1_1S(genNumber2_2(randomGen, deep - 1), genNumber3_2(n - 1, deep))
     else
-      Number1U
+      Number1_1T
   }
-  def genNumber2_2(n: Int, deep: Int): Number2 = {
+  def genNumber2_2(n: Int, deep: Int): Number1_2 = {
     if (n > 0)
-      Number2S(genNumber1_2(randomGen, deep), genNumber2_2(n - 1, deep - 1))
+      Number1_2S(genNumber1_2(randomGen, deep - 1), genNumber3_2(n - 1, deep))
     else
-      Number2U
+      Number1_2T
   }
-  def genNumber3_2(n: Int, deep: Int): Number3 = {
+  def genNumber3_2(n: Int, deep: Int): Number1_3 = {
     if (n > 0)
-      Number3S(genNumber1_2(n - 1, deep), genNumber3_2(n - 1, deep - 1))
+      Number1_3S(genNumber1_2(randomGen, deep - 1), genNumber2_2(n - 1, deep))
     else
-      Number3U
+      Number1_3T
   }
 
   case class Count(num1: Double, num2: Double, num3: Double)
 
-  def countNumber1(num1: Number1): Count = num1 match {
-    case Number1S(tail2, tail3) =>
+  def countNumber1(num1: Number3_1): Count = num1 match {
+    case Number3_1S(tail2, tail3) =>
       val c1 = countNumber2(tail2)
       val c2 = countNumber3(tail3)
       Count(num1 = c1.num1 + c2.num1 + 1, num2 = c1.num2 + c2.num2, num3 = c1.num3 + c2.num3)
-    case Number1T(_) =>
-      Count(num1 = 0, num2 = 0, num3 = 0)
-    case Number1U =>
+    case Number3_1T =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
-  def countNumber2(num2: Number2): Count = num2 match {
-    case Number2S(tail2, tail1) =>
+  def countNumber2(num2: Number3_2): Count = num2 match {
+    case Number3_2S(tail2, tail1) =>
       val c1 = countNumber1(tail2)
-      val c2 = countNumber2(tail1)
+      val c2 = countNumber3(tail1)
       Count(num1 = c1.num1 + c2.num1, num2 = c1.num2 + c2.num2 + 1, num3 = c1.num3 + c2.num3)
-    case Number2T(_) =>
-      Count(num1 = 0, num2 = 0, num3 = 0)
-    case Number2U =>
+    case Number3_2T =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
-  def countNumber3(num3: Number3): Count = num3 match {
-    case Number3S(tail3, tail1) =>
+  def countNumber3(num3: Number3_3): Count = num3 match {
+    case Number3_3S(tail3, tail1) =>
       val c1 = countNumber1(tail3)
-      val c2 = countNumber3(tail1)
+      val c2 = countNumber2(tail1)
       Count(num1 = c1.num1 + c2.num1, num2 = c1.num2 + c2.num2, num3 = c1.num3 + c2.num3 + 1)
-    case Number3T(_) =>
-      Count(num1 = 0, num2 = 0, num3 = 0)
-    case Number3U =>
+    case Number3_3T =>
       Count(num1 = 0, num2 = 0, num3 = 0)
   }
 
   def main(arr: Array[String]): Unit = {
-    lazy val num1: Number1 = genNumber1(10, 3, num1, num2, num3)
-    lazy val num2: Number2 = genNumber2(10, 3, num1, num2, num3)
-    lazy val num3: Number3 = genNumber3(10, 3, num1, num2, num3)
+    lazy val num1: Number2_1 = genNumber1(10, 3, num1, num2, num3)
+    lazy val num2: Number2_2 = genNumber2(10, 3, num1, num2, num3)
+    lazy val num3: Number2_3 = genNumber3(10, 3, num1, num2, num3)
 
-    val num2_1: Number3 = genNumber3_2(10, 3)
+    val num2_1: Number1_1 = genNumber1_2(10, 3)
 
-    println(countNumber2(num2))
-    println(countNumber3(num3))
+    // println(countNumber2(num2))
+    // println(countNumber3(num3))
 
-    println(countNumber3(num2_1))
+    // println(countNumber3(num2_1))
 
-    println(countNumber2(num2.method2(num2_1)))
+    println(countNumber3(num2.method2(num2_1)))
 
-    val a1   = countNumber2(num2).num1 / (countNumber2(num2).num2 - countNumber2(num2).num3)
+    /*val a1   = countNumber2(num2).num1 / (countNumber2(num2).num2 - countNumber2(num2).num3)
     val a1_2 = countNumber1(num1).num1 / (countNumber1(num1).num2 - countNumber1(num1).num3)
     val a2   = countNumber3(num3).num1 / (countNumber3(num3).num2 - countNumber3(num3).num3)
     println("Be x num 1 1", a1)
@@ -121,30 +115,30 @@ object Runner {
     println("x num 1 3", a11_2)
 
     val b1 = List(a1, a2, a3, a4, a5, a6, a1_2, a3_2, a5_2)
-    val b2 = List(a7_2, a9_2, a11_2)
+    val b2 = List(a7_2, a9_2, a11_2)*/
 
-    val c1 = countNumber2(num2.method2(num2_1)).num1 / (countNumber2(num2.method2(num2_1)).num2 - countNumber2(num2.method2(num2_1)).num3)
+    val c1 = countNumber3(num2.method2(num2_1)).num1 / (countNumber3(num2.method2(num2_1)).num2 - countNumber3(num2.method2(num2_1)).num3)
     println("s num 1 1", c1)
-    val c2 = countNumber2(num2.method2(num2_1)).num2 / (countNumber2(num2.method2(num2_1)).num1 - countNumber2(num2.method2(num2_1)).num3)
+    val c2 = countNumber3(num2.method2(num2_1)).num2 / (countNumber3(num2.method2(num2_1)).num1 - countNumber3(num2.method2(num2_1)).num3)
     println("s num 1 2", c2)
-    val c3 = countNumber2(num2.method2(num2_1)).num3 / (countNumber2(num2.method2(num2_1)).num1 - countNumber2(num2.method2(num2_1)).num2)
+    val c3 = countNumber3(num2.method2(num2_1)).num3 / (countNumber3(num2.method2(num2_1)).num1 - countNumber3(num2.method2(num2_1)).num2)
     println("s num 1 3", c3)
 
-    val c4 = countNumber3(num2_1.method3(num2)).num1 / (countNumber3(num2_1.method3(num2)).num2 - countNumber3(num2_1.method3(num2)).num3)
+    val c4 = countNumber3(num2_1.method1(num2)).num1 / (countNumber3(num2_1.method1(num2)).num2 - countNumber3(num2_1.method1(num2)).num3)
     println("s num 1 1", c4)
-    val c5 = countNumber3(num2_1.method3(num2)).num2 / (countNumber3(num2_1.method3(num2)).num1 - countNumber3(num2_1.method3(num2)).num3)
+    val c5 = countNumber3(num2_1.method1(num2)).num2 / (countNumber3(num2_1.method1(num2)).num1 - countNumber3(num2_1.method1(num2)).num3)
     println("s num 1 2", c5)
-    val c6 = countNumber3(num2_1.method3(num2)).num3 / (countNumber3(num2_1.method3(num2)).num1 - countNumber3(num2_1.method3(num2)).num2)
+    val c6 = countNumber3(num2_1.method1(num2)).num3 / (countNumber3(num2_1.method1(num2)).num1 - countNumber3(num2_1.method1(num2)).num2)
     println("s num 1 3", c6)
 
     val d1 = List(c1, c2, c3, c4, c5, c6)
-    for {
+    /*for {
       i1 <- b1
       i2 <- b2
     } yield {
       println(i1 * i2)
       d1.filter(t => math.abs(i1 * i2 - t) < 0.01).foreach(s => println("match", i1 * i2, s))
-    }
+    }*/
   }
 
 }
