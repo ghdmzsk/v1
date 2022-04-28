8e7a745e8265bc266c291aec3b592f2708b71d28
djx314 <djx314@sina.cn>
2022-04-29 05:26:27 +0800

    Godzilla.

--- a/a14/src/main/scala/a14/Counter.scala
+++ a/a14/src/main/scala/a14/Counter.scala
@@ -1,14 +1,42 @@
 package a14
 
-import scala.reflect.runtime.{universe => ru}
+trait Number1_1 {
+  def method1(num2: Number2): Number3_1
+}
+case class Number1_1S(tail1: Number1_1, tail2: Number1_2) extends Number1_1 {
+  override def method1(num2: Number2): Number3_1 = Number3_1U(num2.method3(tail1), tail2.method2(num2))
+}
+case object Number1_1T extends Number1_1 {
+  override def method1(num2: Number2): Number3_1 = Number3_1T
+}
 
-trait Number1
-case class Number1S(head1: () => Number2, head2: () => Number3, instance: ru.TypeTag[_]) extends Number1
+trait Number1_2 {
+  def method2(num2: Number2): Number3_2
+}
+case class Number1_2S(tail2: Number1_2, tail1: Number1_1) extends Number1_2 {
+  override def method2(num2: Number2): Number3_2 = Number3_2S(tail2.method2(num2), tail1.method1(num2))
+}
+case object Number1_2T extends Number1_2 {
+  override def method2(num2: Number2): Number3_2 = Number3_2T
+}
 
-trait Number2
-case class Number2S(tail: Number2, head: Number1) extends Number2
-case object Number2T                              extends Number2
+// ====
+trait Number2 {
+  def method3(num1: Number1_1): Number3_1
+}
+case class Number2S(tail: Number2) extends Number2 {
+  override def method3(num1: Number1_1): Number3_1 = Number3_1S(tail.method3(num1), Number3_2T)
+}
+case class Number2T(tail: () => Number2) extends Number2 {
+  override def method3(num1: Number1_1): Number3_1 = num1.method1(tail())
+}
 
-trait Number3
-case class Number3S(tail: Number3, head: Number1) extends Number3
-case object Number3T                              extends Number3
+// ====
+trait Number3_1
+case class Number3_1S(tail1: Number3_1, tail2: Number3_2) extends Number3_1
+case class Number3_1U(tail1: Number3_1, tail2: Number3_2) extends Number3_1
+case object Number3_1T                                    extends Number3_1
+
+trait Number3_2
+case class Number3_2S(tail2: Number3_2, tail3: Number3_1) extends Number3_2
+case object Number3_2T                                    extends Number3_2
---
+++ a/a15/build.sbt
@@ -0,0 +1,4 @@
+Settings.settings
+name := "a14"
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
