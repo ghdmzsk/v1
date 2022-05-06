c64a38b71e9edba48e317fadaff1d61c9c9e71e5
djx314 <djx314@sina.cn>
2022-05-06 16:12:05 +0800

    slay stay.

--- a/a08/src/main/scala/a08/Runner.scala
+++ a/a08/src/main/scala/a08/Runner.scala
@@ -5,7 +5,7 @@ object Runner {
   lazy val number1ZeroU: Number1 = Number1U(() => number1ZeroU)
 
   def number1Gen1(n: Int): Number1                   = if (n > 0) Number1T(number1Gen1(n - 1)) else Number1S(() => number1ZeroU)
-  def number1Gen2(n: Int): Number1                   = if (n > 0) Number1S(() => number1Gen2(n - 1)) else Number1S(() => number1ZeroU)
+  def number1Gen2(n: Int): Number1                   = if (n > 0) Number1S(() => number1Gen2(n - 1)) else number1ZeroU
   def number1Gen3(n: Int, zero: => Number1): Number1 = if (n > 0) Number1T(number1Gen3(n - 1, zero)) else zero
   def number2Gen(n: Int): Number2                    = if (n > 0) Number2S(number2Gen(n - 1)) else Number2T
 
@@ -37,7 +37,7 @@ object Runner {
         val number2 = number2Gen(i2)
         val number3 = number1.method1(number2)
         val count1  = count(number3)
-        val result1 = if (i2 - i1 > 0) i2 - i1 else 0
+        val result1 = if (i2 - i1 + 1 > 0) i2 - i1 + 1 else 0
         assert(count1 == result1)
       }
     }
--- a/a09/src/main/scala/a09/Runner.scala
+++ a/a09/src/main/scala/a09/Runner.scala
@@ -6,7 +6,7 @@ object Runner {
 
   def number1Gen1(n: Int): Number1                   = if (n > 0) Number1T(number1Gen1(n - 1)) else Number1S(() => number1ZeroU)
   def number1Gen2(n: Int): Number1                   = if (n > 0) Number1T(number1Gen2(n - 1)) else number1ZeroU
-  def number1Gen3(n: Int): Number1                   = if (n > 0) Number1S(() => number1Gen3(n - 1)) else Number1S(() => number1ZeroU)
+  def number1Gen3(n: Int): Number1                   = if (n > 0) Number1S(() => number1Gen3(n - 1)) else number1ZeroU
   def number1Gen4(n: Int): Number1                   = if (n > 0) Number1S(() => number1Gen4(n - 1)) else number1ZeroU
   def number1Gen5(n: Int, zero: => Number1): Number1 = if (n > 0) Number1T(number1Gen5(n - 1, zero)) else zero
   def number1Gen6(n: Int, zero: => Number1): Number1 = if (n > 0) Number1S(() => number1Gen6(n - 1, zero)) else zero
@@ -59,7 +59,7 @@ object Runner {
           val number2 = number2Gen(i2)
           val number3 = number1.method1(number2)
           val count1  = count(number3)
-          val result1 = if (i2 - i1 > 0) i2 - i1 else 0
+          val result1 = if (i2 - i1 + 1 > 0) i2 - i1 + 1 else 0
           assert(count1 == result1)
         }
       }
