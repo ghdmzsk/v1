3606602b8a972e9d1f9dc12bf4eabcdb278a0265
djx314 <djx314@sina.cn>
2022-05-14 20:33:46 +0800

    Commit

--- a/a06/src/main/scala/a06/Counter.scala
+++ a/a06/src/main/scala/a06/Counter.scala
@@ -4,20 +4,22 @@ trait Number1_1 {
   def method1(num2: Number2_1): Number3_1
 }
 case class Number1_1S(tail1: Number1_1, tail2: Number1_2) extends Number1_1 {
-  override def method1(num2: Number2_1): Number3_1 = Number3_1S(num2.method3(tail1, tail2))
+  override def method1(num2: Number2_1): Number3_1 = Number3_1S(() => num2.method3(tail1, tail2), false, false)
 }
-case object Number1_1T extends Number1_1 {
-  override def method1(num2: Number2_1): Number3_1 = Number3_1U
+case class Number1_1T(tail1: () => Number1_1, tail2: () => Number1_2) extends Number1_1 {
+  override def method1(num2: Number2_1): Number3_1 = num2.method3(tail1(), tail2())
 }
 
 trait Number1_2 {
   def method2(num2_1: Number2_1, num2_2: Number2_2): Number3_2
 }
 case class Number1_2S(tail2: Number1_2, tail1: Number1_1) extends Number1_2 {
-  override def method2(num2_1: Number2_1, num2_2: Number2_2): Number3_2 = Number3_2T(num2_2.method3(tail2), tail1.method1(num2_1))
+  override def method2(num2_1: Number2_1, num2_2: Number2_2): Number3_2 =
+    Number3_2T(() => num2_2.method4(tail2), () => tail1.method1(num2_1), false, false)
 }
-case object Number1_2T extends Number1_2 {
-  override def method2(num2_1: Number2_1, num2_2: Number2_2): Number3_2 = Number3_2U
+case class Number1_2T(tail2: () => Number1_2, tail1: () => Number1_1) extends Number1_2 {
+  override def method2(num2_1: Number2_1, num2_2: Number2_2): Number3_2 =
+    Number3_2T(() => num2_2.method4(tail2()), () => Number3_1S(() => tail1().method1(num2_1), true, false), false, true)
 }
 
 // ====
@@ -26,30 +28,28 @@ trait Number2_1 {
 }
 case class Number2_1S(tail1: Number2_1, tail2: Number2_2) extends Number2_1 {
   override def method3(num1_1: Number1_1, num1_2: Number1_2): Number3_1 =
-    Number3_1T(num1_1.method1(tail1), tail2.method3(num1_2))
+    Number3_1T(() => num1_1.method1(tail1), () => tail2.method4(num1_2), false, false)
 }
 case class Number2_1T(tail1: () => Number2_1, tail2: () => Number2_2) extends Number2_1 {
   override def method3(num1_1: Number1_1, num1_2: Number1_2): Number3_1 =
-    Number3_1T(num1_1.method1(tail1()), Number3_2S(tail2().method3(num1_2)))
+    Number3_1T(() => num1_1.method1(tail1()), () => Number3_2S(() => tail2().method4(num1_2), false, true), true, false)
 }
 
 trait Number2_2 {
-  def method3(num1_2: Number1_2): Number3_2
+  def method4(num1_2: Number1_2): Number3_2
 }
 case class Number2_2S(tail2: Number2_2, tail1: Number2_1) extends Number2_2 {
-  override def method3(num1_2: Number1_2): Number3_2 = Number3_2S(num1_2.method2(tail1, tail2))
+  override def method4(num1_2: Number1_2): Number3_2 = Number3_2S(() => num1_2.method2(tail1, tail2), false, false)
 }
 case class Number2_2T(tail2: () => Number2_2, tail1: () => Number2_1) extends Number2_2 {
-  override def method3(num1_2: Number1_2): Number3_2 = num1_2.method2(tail1(), tail2())
+  override def method4(num1_2: Number1_2): Number3_2 = num1_2.method2(tail1(), tail2())
 }
 
 // ====
 trait Number3_1
-case class Number3_1S(tail1: Number3_1)                   extends Number3_1
-case class Number3_1T(tail1: Number3_1, tail2: Number3_2) extends Number3_1
-case object Number3_1U                                    extends Number3_1
+case class Number3_1S(tail1: () => Number3_1, end1: Boolean, end2: Boolean)                         extends Number3_1
+case class Number3_1T(tail1: () => Number3_1, tail2: () => Number3_2, end1: Boolean, end2: Boolean) extends Number3_1
 
 trait Number3_2
-case class Number3_2S(tail2: Number3_2)                   extends Number3_2
-case class Number3_2T(tail2: Number3_2, tail1: Number3_1) extends Number3_2
-case object Number3_2U                                    extends Number3_2
+case class Number3_2S(tail2: () => Number3_2, end1: Boolean, end2: Boolean)                         extends Number3_2
+case class Number3_2T(tail2: () => Number3_2, tail1: () => Number3_1, end1: Boolean, end2: Boolean) extends Number3_2
--- a/a06/src/main/scala/a06/Runner.scala
+++ a/a06/src/main/scala/a06/Runner.scala
@@ -7,39 +7,37 @@ object Runner {
   def randomGen: Int = math.abs(Random.nextInt() % 10)
 
   object GenNumber1 {
-    def gen1(n1: Int, n2: Int, deep: Int): Number1_1 = {
+    def gen1(n1: Int, n2: Int, deep: Int, tail1: => Number1_1, tail2: => Number1_2): Number1_1 = {
       if (deep > 0 && n1 > 0) {
         val m1    = randomGen
         val m2    = randomGen
         val left1 = if (n1 - 1 - m1 > 0) n1 - 1 - m1 else 0
         val left2 = if (n2 - m1 > 0) n2 - m1 else 0
-        Number1_1S(gen1(n1 - left1 - 1 + m2, n2 - left2 + m2, deep - 1), gen2(left1 + m2, left2 + m2, deep - 1))
+        Number1_1S(gen1(n1 - left1 - 1 + m2, n2 - left2 + m2, deep - 1, tail1, tail2), gen2(left1 + m2, left2 + m2, deep - 1, tail1, tail2))
       } else {
-        if (n1 > 0) {
-          Number1_1S(gen1(n1 - 1, n2, deep), Number1_2T)
-        } else if (n2 > 0) {
-          Number1_1S(Number1_1T, Number1_2S(gen2(n1, n2, deep), Number1_1T))
-        } else {
-          Number1_1T
-        }
+        if (n1 > 0)
+          Number1_1S(gen1(n1 - 1, n2, deep, tail1, tail2), tail2)
+        else if (n2 > 0)
+          Number1_1S(tail1, Number1_2S(gen2(n1, n2, deep, tail1, tail2), tail1))
+        else
+          tail1
       }
     }
 
-    def gen2(n1: Int, n2: Int, deep: Int): Number1_2 = {
+    def gen2(n1: Int, n2: Int, deep: Int, tail1: => Number1_1, tail2: => Number1_2): Number1_2 = {
       if (deep > 0 && n2 > 0) {
         val m1    = randomGen
         val m2    = randomGen
         val left1 = if (n1 - m1 > 0) n1 - m1 else 0
         val left2 = if (n2 - 1 - m1 > 0) n2 - 1 - m1 else 0
-        Number1_2S(gen2(n1 - left1 + m2, n2 - left2 - 1 + m2, deep - 1), gen1(left1 + m2, left2 + m2, deep - 1))
+        Number1_2S(gen2(n1 - left1 + m2, n2 - left2 - 1 + m2, deep - 1, tail1, tail2), gen1(left1 + m2, left2 + m2, deep - 1, tail1, tail2))
       } else {
-        if (n2 > 0) {
-          Number1_2S(gen2(n1, n2 - 1, deep), Number1_1T)
-        } else if (n1 > 0) {
-          Number1_2S(Number1_2T, Number1_1S(gen1(n1, n2, deep), Number1_2T))
-        } else {
-          Number1_2T
-        }
+        if (n2 > 0)
+          Number1_2S(gen2(n1, n2 - 1, deep, tail1, tail2), tail1)
+        else if (n1 > 0)
+          Number1_2S(tail2, Number1_1S(gen1(n1, n2, deep, tail1, tail2), tail2))
+        else
+          tail2
       }
     }
   }
@@ -53,13 +51,12 @@ object Runner {
         val left2 = if (n2 - m1 > 0) n2 - m1 else 0
         Number2_1S(gen1(n1 - left1 - 1 + m2, n2 - left2 + m2, deep - 1, tail1, tail2), gen2(left1 + m2, left2 + m2, deep - 1, tail1, tail2))
       } else {
-        if (n1 > 0) {
+        if (n1 > 0)
           Number2_1S(gen1(n1 - 1, n2, deep, tail1, tail2), tail2)
-        } else if (n2 > 0) {
+        else if (n2 > 0)
           Number2_1S(tail1, Number2_2S(gen2(n1, n2, deep, tail1, tail2), tail1))
-        } else {
+        else
           tail1
-        }
       }
     }
 
@@ -71,13 +68,12 @@ object Runner {
         val left2 = if (n2 - 1 - m1 > 0) n2 - 1 - m1 else 0
         Number2_2S(gen2(n1 - left1 + m2, n2 - left2 - 1 + m2, deep - 1, tail1, tail2), gen1(left1 + m2, left2 + m2, deep - 1, tail1, tail2))
       } else {
-        if (n2 > 0) {
+        if (n2 > 0)
           Number2_2S(gen2(n1, n2 - 1, deep, tail1, tail2), tail1)
-        } else if (n1 > 0) {
+        else if (n1 > 0)
           Number2_2S(tail2, Number2_1S(gen1(n1, n2, deep, tail1, tail2), tail2))
-        } else {
+        else
           tail2
-        }
       }
     }
   }
@@ -95,7 +91,7 @@ object Runner {
         val n1 = count1(tail1)
         val n2 = count2(tail2)
         n1.plus(n2).upI1
-      case Number1_1T =>
+      case Number1_1T(_, _) =>
         Num(i1 = 0, i2 = 0)
     }
 
@@ -104,7 +100,7 @@ object Runner {
         val n1 = count1(tail1)
         val n2 = count2(tail2)
         n1.plus(n2).upI2
-      case Number1_2T =>
+      case Number1_2T(_, _) =>
         Num(i1 = 0, i2 = 0)
     }
   }
@@ -130,74 +126,85 @@ object Runner {
   }
 
   object resultCount {
-    def count1(num1: Number3_1): Num = num1 match {
-      case Number3_1S(tail) =>
-        val n1 = count1(tail)
-        n1.upI1
-      case Number3_1T(tail1, tail2) =>
-        val n1 = count1(tail1)
-        val n2 = count2(tail2)
-        n1.plus(n2).upI1
-      case Number3_1U =>
-        Num(i1 = 0, i2 = 0)
+    def count1(num1: Number3_1, end1: Boolean, end2: Boolean): Num = num1 match {
+      case Number3_1S(tail, endr1, endr2) =>
+        if (end1 && endr1)
+          Num(i1 = 0, i2 = 0)
+        else {
+          val n1 = count1(tail(), endr1, endr2)
+          n1.upI1
+        }
+      case Number3_1T(tail1, tail2, endr1, endr2) =>
+        if (end1 && endr1)
+          Num(i1 = 0, i2 = 0)
+        else {
+          val (n1, n2) = (count1(tail1(), endr1, endr2), count2(tail2(), endr1, endr2))
+          n1.plus(n2).upI1
+        }
     }
 
-    def count2(num2: Number3_2): Num = num2 match {
-      case Number3_2S(tail) =>
-        val n1 = count2(tail)
-        n1.upI2
-      case Number3_2T(tail2, tail1) =>
-        val n1 = count2(tail2)
-        val n2 = count1(tail1)
-        n1.plus(n2).upI2
-      case Number3_2U =>
-        Num(i1 = 0, i2 = 0)
+    def count2(num2: Number3_2, end1: Boolean, end2: Boolean): Num = num2 match {
+      case Number3_2S(tail, endr1, endr2) =>
+        if (end2 && endr2)
+          Num(i1 = 0, i2 = 0)
+        else {
+          val n1 = count2(tail(), endr1, endr2)
+          n1.upI2
+        }
+      case Number3_2T(tail2, tail1, endr1, endr2) =>
+        if (end2 && endr2)
+          Num(i1 = 0, i2 = 0)
+        else {
+          val (n1, n2) = (count1(tail1(), endr1, endr2), count2(tail2(), endr1, endr2))
+          n1.plus(n2).upI2
+        }
     }
   }
 
   def main(arr: Array[String]): Unit = {
     for {
-      i1 <- 1 to 10
-      i2 <- 1 to 10
-      i3 <- 1 to 10
-      i4 <- 1 to 10
+      i1 <- -20 to 20
+      i2 <- -20 to 20
       i5 <- 1 to 5
     } {
-      val num1_1: Number1_1 = GenNumber1.gen1(i1, i2, i5)
-      val num1_2: Number1_2 = GenNumber1.gen2(i1, i2, i5)
+      val m1 = randomGen
+      val m2 = randomGen
+      val m3 = randomGen
+      val m4 = randomGen
+
+      lazy val num1_1Zero: Number1_1 = Number1_1T(() => num1_1Zero, () => num1_2Zero)
+      lazy val num1_2Zero: Number1_2 = Number1_2T(() => num1_2Zero, () => num1_1Zero)
 
       lazy val num2_1Zero: Number2_1 = Number2_1T(() => num2_1Zero, () => num2_2Zero)
       lazy val num2_2Zero: Number2_2 = Number2_2T(() => num2_2Zero, () => num2_1Zero)
 
-      val num2_1: Number2_1 = GenNumber2.gen1(i3, i4, i5, num2_1Zero, num2_2Zero)
-      val num2_2: Number2_2 = GenNumber2.gen2(i3, i4, i5, num2_1Zero, num2_2Zero)
+      val num1_1: Number1_1 =
+        if (i1 > 0) GenNumber1.gen1(m1 + i1, m1, i5, num1_1Zero, num1_2Zero) else GenNumber1.gen1(m1, m1 - i1, i5, num1_1Zero, num1_2Zero)
+      val num1_2: Number1_2 =
+        if (i1 > 0) GenNumber1.gen2(m1 + i1, m1, i5, num1_1Zero, num1_2Zero) else GenNumber1.gen2(m1, m1 - i1, i5, num1_1Zero, num1_2Zero)
+
+      val num2_1: Number2_1 =
+        if (i2 > 0) GenNumber2.gen1(m1 + i2, m1, i5, num2_1Zero, num2_2Zero) else GenNumber2.gen1(m1, m1 - i2, i5, num2_1Zero, num2_2Zero)
+      val num2_2: Number2_2 =
+        if (i2 > 0) GenNumber2.gen2(m1 + i2, m1, i5, num2_1Zero, num2_2Zero) else GenNumber2.gen2(m1, m1 - i2, i5, num2_1Zero, num2_2Zero)
 
       val count1 = paramCount1.count1(num1_1)
       val count2 = paramCount2.count1(num2_1)
 
-      val count3 = resultCount.count1(num1_1.method1(num2_1))
+      val count3 = resultCount.count1(num1_1.method1(num2_1), false, false)
 
       val count4 = paramCount1.count2(num1_2)
       val count5 = paramCount2.count2(num2_2)
 
-      val count6 = resultCount.count2(num2_2.method3(num1_2))
+      val count6 = resultCount.count2(num2_2.method4(num1_2), false, false)
 
-      println("// ====================")
-      println(count1.value, count2.value, count1.plus(count2).value, count3.value)
-      println(count4.value, count5.value, count4.plus(count5).value, count6.value)
+      assert(count1.value == i1)
+      assert(count2.value == i2)
+      assert(count3.value == count1.value + count2.value)
 
-      /*if (count1.plus(count2).value != count3.value) {
-        println(count1.value, count2.value, count1.plus(count2).value, count3.value)
-      } else {
-        println(true)
-      }*/
-
-      /*if (count4.plus(count5).value != count6.value) {
-        println(count4.value, count5.value, count4.plus(count5).value, count6.value)
-      } else {
-        println(true)
-      }*/
-
+      assert(count4.value == i1)
+      assert(count5.value == i2)
+      assert(count6.value == count4.value + count5.value)
     }
 
   }
