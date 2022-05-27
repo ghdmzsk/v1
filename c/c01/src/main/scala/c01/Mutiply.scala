424fd3d3e3d5fe73d4d0a33ea51bb039b8ce0f80
djx314 <djx314@sina.cn>
2022-05-27 14:13:09 +0800

    Commit

--- c/c01/src/main/scala/c01/Mutiply.scala
+++ c/c01/src/main/scala/c01/Mutiply.scala
@@ -11,7 +11,8 @@ object Mutiply {
       append(new Number1T)
     }
 
-    def genNum2(n: Int, zero: => Number2): Number2 = if (n > 0) Number2S(genNum2(n - 1, zero)) else zero
+    def genNum2_1(n: Int, zero: => Number2): Number2 = if (n > 0) Number2S(genNum2_1(n - 1, zero)) else zero
+    def genNum2_2(n: Int, zero: => Number2): Number2 = if (n > 0) Number2T(() => genNum2_2(n - 1, zero)) else zero
 
     def countNumber1: Int = {
       var i  = 0
@@ -23,10 +24,17 @@ object Mutiply {
       i
     }
 
-    def countNumber2(n: Number2): Int = {
+    def countNumber2_1(n: Number2): Int = {
       n match {
-        case Number2S(tail) => countNumber2(tail) + 1
+        case Number2S(tail) => countNumber2_1(tail) + 1
         case Number2T(_)    => 0
+      }
+    }
+
+    def countNumber2_2(n: Number2): Int = {
+      n match {
+        case Number2T(tail) => countNumber2_2(tail()) + 1
+        case Number2S(_)    => 0
       }
     }
 
@@ -38,14 +46,92 @@ object Mutiply {
         i1 <- 0 to 20
         i2 <- 0 to 20
       } {
-        val context1: MutiplyCounter = new MutiplyCounter
-        context1.genNum1(i1)
-        lazy val n2: context1.Number2     = context1.genNum2(i2, n2Zero)
-        lazy val n2Zero: context1.Number2 = context1.Number2T(() => n2)
-        assert(context1.countNumber1 == i1)
-        context1.num1.method1(n2)
-        assert(context1.countNumber2(n2) == i2)
-        assert(context1.countNumber1 == i1 * i2)
+        {
+          // 乘法
+          {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_1(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2T(() => n2)
+            assert(context1.countNumber1 == i1)
+            context1.num1.method1(n2)
+            assert(context1.countNumber2_1(n2) == i2)
+            assert(context1.countNumber1 == i1 * i2)
+          }
+          {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_1(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2T(() => n2)
+            assert(context1.countNumber1 == i1)
+            n2Zero.method2
+            assert(context1.countNumber2_1(n2) == i2)
+            assert(context1.countNumber1 == i1 * i2)
+          }
+          if (i1 > 0) {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_1(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2T(() => n2)
+            assert(context1.countNumber1 == i1)
+            context1.num1.method1(n2Zero)
+            assert(context1.countNumber2_1(n2) == i2)
+            assert(context1.countNumber1 == i1 * i2 - i2)
+          }
+          {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_1(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2T(() => n2)
+            assert(context1.countNumber1 == i1)
+            n2.method2
+            assert(context1.countNumber2_1(n2) == i2)
+            assert(context1.countNumber1 == i1 * i2 + i2)
+          }
+        }
+        {
+          // 除法
+          if (i2 > 0) {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_2(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2S(n2)
+            assert(context1.countNumber1 == i1)
+            context1.num1.method1(n2Zero)
+            assert(context1.countNumber2_2(n2) == i2)
+            assert(context1.countNumber1 == (i1 + i2 - 1) / i2)
+          }
+          if (i2 > 0) {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_2(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2S(n2)
+            assert(context1.countNumber1 == i1)
+            n2.method2
+            assert(context1.countNumber2_2(n2) == i2)
+            assert(context1.countNumber1 == i1 / i2)
+          }
+          if (i1 > 0 && i2 > 0) {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_2(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2S(n2)
+            assert(context1.countNumber1 == i1)
+            context1.num1.method1(n2)
+            assert(context1.countNumber2_2(n2) == i2)
+            assert(context1.countNumber1 == (i1 - 1) / i2)
+          }
+          if (i2 > 0) {
+            val context1: MutiplyCounter = new MutiplyCounter
+            context1.genNum1(i1)
+            lazy val n2: context1.Number2     = context1.genNum2_2(i2, n2Zero)
+            lazy val n2Zero: context1.Number2 = context1.Number2S(n2)
+            assert(context1.countNumber1 == i1)
+            n2Zero.method2
+            assert(context1.countNumber2_2(n2) == i2)
+            assert(context1.countNumber1 == (i1 + i2) / i2)
+          }
+        }
       }
     }
   }
