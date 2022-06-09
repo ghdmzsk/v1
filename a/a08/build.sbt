6a2cb0c140b6f78db3f37d845c2115ae79357321
djx314 <djx314@sina.cn>
2022-06-09 19:54:50 +0800

    law 法律法规， raw 原始的，未加工的。

--- a/a08/build.sbt
+++ a/a08/build.sbt
@@ -5,3 +5,4 @@ javaOptions += "-Xss180k"
 // run / fork := true
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.19"
+libraryDependencies += "org.slf4j"          % "slf4j-simple"     % "1.7.36"
--- a/a08/src/main/scala/a08/Runner.scala
+++ a/a08/src/main/scala/a08/Runner.scala
@@ -73,63 +73,43 @@ object Runner {
       }
     }
 
-    /*{
+    {
       // 乘法
       for {
         i1 <- 0 to 20
         i2 <- 0 to 20
       } {
-        val countNum1 = {
+        {
           lazy val number1s: Number1 = number1Gen5(i1, number1t)
-          lazy val number1t: Number1 = Number1S(() => number1s)
+          lazy val number1t: Number1 = Number1S(() => Future(number1s))
           val number2: Number2       = number2Gen(i2)
-          def number3                = number2.method2(number1s)
-          val count1                 = count(() => number3)
-          val result1                = i1 * i2
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, i1 * i2, "乘法1")(countContext)
+          number2.method2(number1s, result1)
         }
 
-        val countNum2 = {
+        {
           lazy val number1s: Number1 = number1Gen5(i1, number1t)
-          lazy val number1t: Number1 = Number1S(() => number1s)
+          lazy val number1t: Number1 = Number1S(() => Future(number1s))
           val number2: Number2       = number2Gen(i2)
-          def number3                = number1t.method1(number2)
-          val count1                 = count(() => number3)
-          val result1                = i1 * i2
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, i1 * i2, "乘法2")(countContext)
+          number1t.method1(number2, result1)
         }
 
-        val countNum3 = {
+        {
           lazy val number1s: Number1 = number1Gen5(i1, number1t)
-          lazy val number1t: Number1 = Number1S(() => number1s)
+          lazy val number1t: Number1 = Number1S(() => Future(number1s))
           val number2: Number2       = number2Gen(i2)
-          def number3                = number1s.method1(number2)
-          val count1                 = count(() => number3)
-          val result1                = i1 * (i2 + 1)
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, i1 * (i2 + 1), "乘法3")(countContext)
+          number1s.method1(number2, result1)
         }
 
-        val countNum4 = {
+        {
           lazy val number1s: Number1 = number1Gen5(i1, number1t)
-          lazy val number1t: Number1 = Number1S(() => number1s)
+          lazy val number1t: Number1 = Number1S(() => Future(number1s))
           val number2: Number2       = number2Gen(i2)
-          def number3                = number2.method2(number1t)
-          val count1                 = count(() => number3)
-          val result1                = if (i2 == 0) 0 else i1 * (i2 - 1)
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, if (i2 == 0) 0 else i1 * (i2 - 1), "乘法4")(countContext)
+          number2.method2(number1t, result1)
         }
-
-        if (i2 == 0)
-          assert(countNum2 == countNum4)
-        else
-          assert(countNum2 == countNum4 + i1)
-
-        assert(countNum1 == countNum2)
-        assert(countNum1 == countNum3 - i1)
       }
     }
 
@@ -139,64 +119,39 @@ object Runner {
         i1 <- 1 to 20
         i2 <- 0 to 20
       } {
-        val countNum1 = {
+        {
           lazy val number1s: Number1 = number1Gen6(i1, number1t)
-          lazy val number1t: Number1 = Number1T(number1s)
+          lazy val number1t: Number1 = Number1T(() => Future(number1s))
           val number2                = number2Gen(i2)
-          def number3                = number1t.method1(number2)
-          val count1                 = count(() => number3)
-          val result1                = (i2 + i1) / i1
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, (i2 + i1) / i1, "除法1")(countContext)
+          number1t.method1(number2, result1)
         }
 
-        val countNum2 = {
+        {
           lazy val number1s: Number1 = number1Gen6(i1, number1t)
-          lazy val number1t: Number1 = Number1T(number1s)
+          lazy val number1t: Number1 = Number1T(() => Future(number1s))
           val number2                = number2Gen(i2)
-          def number3                = number1s.method1(number2)
-          val count1                 = count(() => number3)
-          val result1                = i2 / i1
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, i2 / i1, "除法2")(countContext)
+          number1s.method1(number2, result1)
         }
 
-        val countNum3 = {
+        {
           lazy val number1s: Number1 = number1Gen6(i1, number1t)
-          lazy val number1t: Number1 = Number1T(number1s)
+          lazy val number1t: Number1 = Number1T(() => Future(number1s))
           val number2                = number2Gen(i2)
-          def number3                = number2.method2(number1t)
-          val count1                 = count(() => number3)
-          val result1                = if (i2 % i1 == 0) i2 / i1 else i2 / i1 + 1
-          val result2                = (i2 + i1 - 1) / i1
-          assert(result1 == result2)
-          assert(count1 == result1)
-          count1
+          val result1                = Input(i1, i2, (i2 + i1 - 1) / i1, "除法3")(countContext)
+          number2.method2(number1t, result1)
         }
 
         if (i2 > 0) {
-          val countNum4 = {
-            lazy val number1s: Number1 = number1Gen6(i1, number1t)
-            lazy val number1t: Number1 = Number1T(number1s)
-            val number2                = number2Gen(i2)
-            def number3                = number2.method2(number1s)
-            val count1                 = count(() => number3)
-            val result1                = (i2 - 1) / i1
-            assert(count1 == result1)
-            count1
-          }
-
-          assert(countNum3 == countNum4 + 1)
+          lazy val number1s: Number1 = number1Gen6(i1, number1t)
+          lazy val number1t: Number1 = Number1T(() => Future(number1s))
+          val number2                = number2Gen(i2)
+          val result1                = Input(i1, i2, (i2 - 1) / i1, "除法4")(countContext)
+          number2.method2(number1s, result1)
         }
-
-        if (i2 % i1 == 0)
-          assert(countNum2 == countNum3)
-        else
-          assert(countNum2 == countNum3 - 1)
-
-        assert(countNum2 == countNum1 - 1)
       }
-    }*/
+    }
 
     println("Press any key to count.")
     StdIn.readLine()
