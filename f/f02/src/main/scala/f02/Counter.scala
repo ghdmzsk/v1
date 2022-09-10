ba074a5d34c28188ca90dc6afd81bbc077aa2b28
djx314 <djx314@sina.cn>
2022-09-11 04:56:30 +0800

    typo type.

--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -7,9 +7,9 @@ trait Number3S(using() => Number4) extends Number3 {
 trait Number3T extends Number3
 
 trait Number4 extends (Number3 => Number3 | Number4)
-trait Number4S(var husband: Number3) extends Number4 {
+trait Number4S(val husband: Number3) extends Number4 {
   def birth: Number3 | Number4 = apply(husband)
 }
-trait Number4T(val tail: Number4) extends Number4 {
+trait Number4T(var tail: Number4) extends Number4 {
   override def apply(num3: Number3): Number3 | Number4 = tail(num3)
 }
