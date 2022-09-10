e427f66465afe3e60d055903ca9d49b4535f76b6
djx314 <djx314@sina.cn>
2022-09-11 06:32:37 +0800

    funny fancy.

--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -1,15 +1,14 @@
 package f02
 
 trait Number3
-trait Number3S extends Number3
-trait Number3T extends Number3 {
+trait Number3S extends Number3 {
   lazy val child: Number3
 }
-trait Number3U(using Number4) extends Number3T {
+trait Number3T(using Number4) extends Number3S {
   override lazy val child: Number3
 }
 
-trait Number4(using Number4) extends (Number3 => Number3U | Number4)
+trait Number4(using Number4) extends (Number3 => Number3T | Number4)
 trait Number4S(using Number4)(val husband: () => Number3) extends Number4 {
-  def birth: Number3U | Number4 = apply(husband())
+  def birth: Number3T | Number4 = apply(husband())
 }
