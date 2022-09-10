3f8358865071ab7956e02bef73c9654c9bc804dd
djx314 <djx314@sina.cn>
2022-09-11 04:34:44 +0800

    logic login.

--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -2,8 +2,14 @@ package f02
 
 trait Number3
 trait Number3S(using() => Number4) extends Number3 {
-  def children: Number3 | Number4
+  lazy val child: Number3
 }
+trait Number3T extends Number3
 
-trait Number4
-trait Number4S(val all: (Number3, Number3 | Number4)) extends Number4
+trait Number4 extends (Number3 => Number3 | Number4)
+trait Number4S(var husband: Number3) extends Number4 {
+  def birth: Number3 | Number4 = apply(husband)
+}
+trait Number4T(val tail: Number4) extends Number4 {
+  override def apply(num3: Number3): Number3 | Number4 = tail(num3)
+}
