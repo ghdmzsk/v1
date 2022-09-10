91aa754534a7f9ca397a4e13a4a1cecda088bb86
djx314 <djx314@sina.cn>
2022-09-11 01:05:08 +0800

    filling feeling

--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -1,7 +1,8 @@
 package f02
 
 trait Number3
-trait Number3S(using() => Number4) extends Number3
+trait Number3S(using() => Number4)      extends Number3
+trait Number3T(using Number3 | Number4) extends Number3
 
 trait Number4
 trait Number4S(using Number3, Number3 | Number4) extends Number4
