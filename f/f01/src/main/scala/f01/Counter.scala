24cb51ea92674cfeceda3680acc905820802c8a2
djx314 <djx314@sina.cn>
2022-09-11 06:20:07 +0800

    tigger tiger.

--- f/f01/src/main/scala/f01/Counter.scala
+++ f/f01/src/main/scala/f01/Counter.scala
@@ -10,43 +10,43 @@ object Number3S extends Number3:
   override def size: Int = 0
 
   override lazy val add: Number3 =
-    given (() => Number4)  = () => new Number4S(num3)
+    given Number4          = new Number4S(() => num3)
     lazy val num3: Number3 = new Number3T1
     num3
   end add
 end Number3S
 
 // 考虑后继特性
-trait Number4(next: Number3):
+trait Number4(next: () => Number3):
   def preSize: Int
 end Number4
-class Number4S(next: Number3) extends Number4(next):
+class Number4S(next: () => Number3) extends Number4(next):
   override def preSize: Int = 0
 end Number4S
 
 // 有前驱
-trait Number3T(using() => Number4) extends Number3:
+trait Number3T(using Number4) extends Number3:
   override def size: Int
 
-  lazy val addImpl: Number4 = new Number3ST(add)
+  private lazy val addImpl: Number4 = new Number3ST(() => add)
 
   override lazy val add: Number3 =
-    given (() => Number4)  = () => addImpl
+    given Number4          = addImpl
     lazy val num3: Number3 = new Number3T1
     num3
   end add
 end Number3T
 // 不考虑后继特性
-class Number3T1(using() => Number4) extends Number3T:
-  lazy val pre: Number4  = summon[() => Number4]()
+class Number3T1(using Number4) extends Number3T:
+  lazy val pre: Number4  = summon[Number4]
   override def size: Int = pre.preSize + 1
 end Number3T1
 
 // 既有前驱又考虑后继特性
-class Number3ST(next: Number3)(using() => Number4) extends Number3T, Number4(next):
-  lazy val pre: Number4     = summon[() => Number4]()
+class Number3ST(using Number4)(next: () => Number3) extends Number3T, Number4(next):
+  lazy val pre: Number4     = summon[Number4]
   override def preSize: Int = pre.preSize + 1
-  override def size: Int    = next.size - 1
+  override def size: Int    = next().size - 1
 end Number3ST
 
 object Number3:
--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -1,15 +1,15 @@
 package f02
 
 trait Number3
-trait Number3S(using() => Number4) extends Number3 {
+trait Number3S extends Number3
+trait Number3T extends Number3 {
   lazy val child: Number3
 }
-trait Number3T extends Number3
-
-trait Number4 extends (Number3 => Number3 | Number4)
-trait Number4S(val husband: Number3) extends Number4 {
-  def birth: Number3 | Number4 = apply(husband)
+trait Number3U(using Number4) extends Number3T {
+  override lazy val child: Number3
 }
-trait Number4T(var tail: Number4) extends Number4 {
-  override def apply(num3: Number3): Number3 | Number4 = tail(num3)
+
+trait Number4(using Number4) extends (Number3 => Number3U | Number4)
+trait Number4S(using Number4)(val husband: () => Number3) extends Number4 {
+  def birth: Number3U | Number4 = apply(husband())
 }
