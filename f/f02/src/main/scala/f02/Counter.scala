6b4767c9d733cfffc869e4275347d01f01c10093
djx314 <djx314@sina.cn>
2022-09-11 12:32:13 +0800

    rouse route.

--- f/f02/src/main/scala/f02/Counter.scala
+++ f/f02/src/main/scala/f02/Counter.scala
@@ -1,6 +1,6 @@
 package f02
 
-trait Number3
+trait Number3 extends (Number4 => Number3T | Number4)
 trait Number3S extends Number3:
   lazy val child: Number3
 end Number3S
@@ -8,8 +8,6 @@ trait Number3T(using Number4) extends Number3S:
   override lazy val child: Number3
 end Number3T
 
-trait Number4                 extends (Number3 => Number3T | Number4)
-trait Number4S(using Number4) extends Number4
-trait Number4T(val husband: () => Number3) extends Number4:
-  def birth: Number3T | Number4 = apply(husband())
-end Number4T
+trait Number4                              extends (Number3 => Number3T | Number4)
+trait Number4S(using Number4)              extends Number4
+trait Number4T(val husband: () => Number3) extends Number4
